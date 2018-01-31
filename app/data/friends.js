12deb85314306f704edf2bba3449ab1308b5ec25
Mariela Pascual <marielapascual@Marielas-MBP.the-beach.net>
2018-01-31 18:39:02 -0500

    css and html

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friendsList = [
+   {
+      "name":"",
+      "photo":"http://boredomtherapy.com/wp-content/uploads/2015/02/04-animal-selfies.jpg",
+      "scores":[5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+      "name":"",
+      "photo":"http://boredomtherapy.com/wp-content/uploads/2015/02/08-animal-selfies.jpg",
+      "scores":[4,2,5,1,3,2,2,1,3,2]
+   },
+   {
+      "name":"Mijail",
+      "photo":"http://boredomtherapy.com/wp-content/uploads/2015/02/12-animal-selfies.jpg",
+      "scores":[5,2,2,2,4,1,3,2,5,5]
+   },
+   {
+      "name":"Papichulo",
+      "photo":"http://boredomtherapy.com/wp-content/uploads/2015/02/14-animal-selfies.jpg",
+      "scores":[3,3,4,2,2,1,3,2,2,3]
+   },
+	{
+	   "name":"Fulanita",
+	   "photo":"http://boredomtherapy.com/wp-content/uploads/2015/02/24-animal-selfies.jpg",
+	   "scores":[4,3,4,1,5,2,5,3,1,4]
+	},
+      {
+      "name":"Menganito",
+      "photo":"http://boredomtherapy.com/wp-content/uploads/2015/02/15-animal-selfies.jpg",
+      "scores":[3,3,4,1,4,3,5,3,5,4]
+   },
+	{
+	   "name":"Consuela",
+	   "photo":"http://boredomtherapy.com/wp-content/uploads/2015/02/22-animal-selfies.jpg",
+	   "scores":[4,4,2,3,2,2,3,2,4,5]
+	}
+
+]
+
+module.exports = friends;
